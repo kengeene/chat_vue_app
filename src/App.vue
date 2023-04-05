5339abab1df05a403a3e2506af69bda3c99f57a6
enkuria <eugene@sendyit.com>
2023-04-05 21:32:06 +0300

    feat: add chat display component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
+  <ChatDisplay/>
   <MessageBox/>
 </template>
 <script>
 import MessageBox from './components/MessageBox.vue'
-
+import ChatDisplay from './components/ChatDisplay.vue'
 export default {
   components: {
     MessageBox,
+    ChatDisplay,
   },
 }
 </script>
---
+++ src/components/ChatDisplay.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="display-area">
+        <div v-if="messages.length === 0">
+            No messages here
+        </div>
+        <div v-else v-for="(message, index) in messages" :key="index">
+            {{ message }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Display',
+        props: {
+            messages: {
+                type: Array,
+                default: () => []
+            },
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+.display-area{
+    background-color: #ffffff;
+    height: 85%;
+    width: 100%;
+    display: block;
+    color: #000000;
+}
+</style>
