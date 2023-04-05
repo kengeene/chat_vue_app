22102308f4430ee1d735067e9da8009ac041f87c
enkuria <eugene@sendyit.com>
2023-04-05 16:20:53 +0300

    initial commit

---
+++ src/components/MessageBox.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="footer">
+        <v-form @submit.prevent>
+
+            <v-row>
+                <v-col cols="11">
+                    <v-text-field
+        v-model="message"
+        :rules="rules"
+      ></v-text-field>
+                </v-col>
+                <v-col>
+                    <v-btn type="submit" block class="mt-2">Submit</v-btn>
+                </v-col>
+            </v-row>
+    </v-form>
+
+    </div>
+</template>
+
+<script>
+    export default {
+data() {
+    return {
+        message: '',
+        rules: [(v)=> !!v || 'This field is required']
+    }
+},
+    }
+</script>
+
+<style lang="scss" scoped>
+.footer{
+    display: block;
+    position: fixed;
+    bottom: 0;
+    z-index: 10;
+    width: 100%;
+    left: 0;
+}
+</style>
