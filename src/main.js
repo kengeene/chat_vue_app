67a232945637196e0352ad749de02c5c7a461e2c
enkuria <eugene@sendyit.com>
2023-04-05 22:20:20 +0300

    feat: add Firebase

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+import './utils/firebase.js'
+
 const vuetify = createVuetify({
   components,
   directives,
