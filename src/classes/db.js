2ff2cf7875a75724be70620315e0885c0c15015e
enkuria <eugene@sendyit.com>
2023-04-06 09:21:14 +0300

    refactor: add db class

---
+++ src/classes/db.js
@@ -0,0 +1,34 @@
+import { collection, addDoc, getDocs } from "firebase/firestore";
+
+export default class Database {
+    constructor(collection){
+        this.collection = collection;
+    }
+
+    async add({data}){
+        try {
+  const docRef = await addDoc(collection(db, this.collection), data);
+  console.log("Document written with ID: ", docRef.id);
+  return docRef;
+} catch (e) {
+  return e;
+}
+
+    }
+
+    get(){
+    return new Promise(async(resolve, reject)=>{
+        try{
+            const querySnapshot = await getDocs(collection(db, this.collection));
+            const data = [];
+            querySnapshot.forEach((doc) => {
+                  data.push(doc.data());
+                });
+                resolve(data);
+        }catch(e){
+            reject(e);
+        }
+    });
+
+    }
+}
